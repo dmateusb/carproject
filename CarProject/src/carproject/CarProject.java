7bba1f7f04d1169143fb3340c0e1d50663d9f202
David Mateus <dmateusb@unal.edu.co>
2019-10-23 17:04:45 -0500

    Primera subida

---
+++ CarProject/src/carproject/CarProject.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carproject;
+
+import java.awt.geom.Rectangle2D;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author david
+ */
+public class CarProject extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.initStyle(StageStyle.DECORATED);
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        //stage.setMaximized(true);
+        Screen screen = Screen.getPrimary();
+        javafx.geometry.Rectangle2D bounds = screen.getVisualBounds();
+        stage.setX(bounds.getMinX());
+        stage.setY(bounds.getMinY());
+        stage.setWidth(bounds.getWidth());
+        stage.setHeight(bounds.getHeight());
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ CarProject/src/carproject/HomeController.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carproject;
+import org.jpedal.*;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import org.jpedal.exception.PdfException;
+
+/**
+ *
+ * @author david
+ */
+public class HomeController implements Initializable {
+
+    @FXML
+    private HBox min;
+    @FXML
+    private FontAwesomeIcon close;
+    @FXML
+    private Button btnprueba;
+    @FXML
+    private ImageView paquete1;
+    @FXML
+    private Button btnprueba1;
+    @FXML
+    private Button btnprueba2;
+    @FXML
+    private Button btnprueba3;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    @FXML
+    private void min(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+    @FXML
+    private void closed(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void abrirPdf(MouseEvent event) throws IOException, PdfException {
+        Desktop.getDesktop().open(new java.io.File("./res/pdf/1.pdf"));
+    }
+
+
+
+
+    
+}
