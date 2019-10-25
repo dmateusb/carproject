b852ae56d75d21bebdebd7cd8d5417a2114f5e65
David Mateus <dmateusb@unal.edu.co>
2019-10-25 01:38:17 -0500

    cambio resolucion

--- CarProject/src/carproject/AguaEducacionController.java
+++ CarProject/src/carproject/AguaEducacionController.java
@@ -5,20 +5,24 @@
  */
 package carproject;
 
+import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.ImageCursor;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import org.jpedal.exception.PdfException;
 
 /**
  * FXML Controller class
@@ -35,15 +39,15 @@ public class AguaEducacionController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    @FXML
+    }
+
     public void openFXML() throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        Parent root= (BorderPane) loader.load(getClass().getResource("AguaEducacion.fxml").openStream());
-        Stage stage= new Stage();
-        stage.initOwner(this.stage);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("AguaEducacion.fxml"));
+        Parent root = (BorderPane) loader.load(getClass().getResource("AguaEducacion.fxml").openStream());
+        Stage stage = new Stage();
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        cambiarPuntero(stage);
         stage.setTitle("Detalle");
         stage.centerOnScreen();
         stage.show();
@@ -66,6 +70,16 @@ public class AguaEducacionController implements Initializable {
 
     public void setStage(Stage stage) {
         this.stage = stage;
+    }
+    public void cambiarPuntero(Stage stage) {
+        Image image = new Image("file:./res/img/puntero.png");
+        stage.getScene().setCursor(new ImageCursor(image, image.getWidth() / 2,
+                image.getHeight() / 2));
+    }
+    @FXML
+    private void abrirPdf(MouseEvent event) throws IOException, PdfException {
+        System.out.println("buenas");
+        Desktop.getDesktop().open(new java.io.File("./src/pdf/guiawet.pdf"));
     }
     
     
